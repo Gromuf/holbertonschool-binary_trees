7b17dc5f80ff2c1a23090082484240efae27538b
Beaumois <louis.beaumois@etudiant.univrennes1.fr>
2024-08-05 10:37:02 +0200

    adding task 1 and test file

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,12 +11,18 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newNode;
 
+	if (parent == NULL)
+		reutrn(NULL);
+
 	newNode = malloc(sizeof(binary_tree_t));
+
 	if (newNode == NULL)
 		return (NULL);
+
 	newNode->parent = parent;
 	newNode->left = NULL;
 	newNode->right = NULL;
 	newNode->n = value;
+
 	return (newNode);
 }
