a1412cf101bc59f20516ae485ac7f4c9c6d04a9f
Beaumois <louis.beaumois@etudiant.univrennes1.fr>
2024-08-05 10:40:03 +0200

    removing useless if

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,9 +11,6 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newNode;
 
-	if (parent == NULL)
-		return (NULL);
-
 	newNode = malloc(sizeof(binary_tree_t));
 
 	if (newNode == NULL)
