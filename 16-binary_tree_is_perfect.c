5f080b14bb4b34933ac0ee8f77cfdc14feb02ab2
Beaumois <louis.beaumois@etudiant.univrennes1.fr>
2024-08-05 15:23:57 +0200

    betty

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,22 +1,6 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_perfect - Checks if a binary tree is perfect.
- * @tree: A pointer to the root node of the tree to check.
- *
- * Return: 1 if the tree is perfect, 0 otherwise. If tree is NULL, return 0.
- */
-int binary_tree_is_perfect(const binary_tree_t *tree)
-{
-	if (tree == NULL)
-		return (0);
-
-	int d = depth(tree);
-
-	return (is_perfect_rec(tree, d, 0));
-}
-
-/**
  * depth - Calculates the depth of the leftmost leaf.
  * @tree: A pointer to the root node of the tree.
  *
@@ -53,4 +37,20 @@ int is_perfect_rec(const binary_tree_t *tree, int depth, int level)
 
 	return (is_perfect_rec(tree->left, depth, level + 1) &&
 			is_perfect_rec(tree->right, depth, level + 1));
+}
+
+/**
+ * binary_tree_is_perfect - Checks if a binary tree is perfect.
+ * @tree: A pointer to the root node of the tree to check.
+ *
+ * Return: 1 if the tree is perfect, 0 otherwise. If tree is NULL, return 0.
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	int d = depth(tree);
+
+	return (is_perfect_rec(tree, d, 0));
 }
